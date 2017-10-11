9f86988de7a0e675f57bf3399d32ce9fe97507fb
lkskstlr <lkskstlr@gmail.com>
2017-10-11 16:22:32 +0200

    changed setup etc to get ready for pypi

--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,5 @@
 # Grafeo Python Utils
 
+**This project as well as this documentation are work in progress. The implementation is most likely not cryptographically sound. Do not use this within any production system. Any help is appreciated, just reach out via github issues :)**
+
 Grafeo is a cryptograhpically secured and authenticated database for supply chains. This repo includes python utils to use the underlying protocol.
--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
-setup(name="Grafeo",
-      version="0.1.7",
-      packages=["grafeo"],
-      package_data={'grafeo': ['templates/*.html']}
+setup(
+    name="grafeo",
+    packages=["grafeo"],
+    version="0.0.0",
+    description = 'grafeo: Cryptographically authenticated supply chain storage protocol',
+    author = 'Lukas Köstlers',
+    author_email = 'lkskstlr@gmail.com',
+    url = 'https://github.com/lkskstlr/grafeo-utils',
+    keywords = [],
+    classifiers = [],
 )
